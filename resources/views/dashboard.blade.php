02ce4480c8e5b5a949e992214ad67da433b6e23d
farhan-crescotec <farhan.raza@cresco.org>
2025-02-28 15:19:28 +0400

    Add Icons in Dashboard Modules

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,26 +8,29 @@
             </div>
         @else
             <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 lg:gap-7 w-3/5">
-                <a href="{{ route('reports.' . $page->user->modules->sortBy('order')->first()->slug) }}"
-                   class="animate-in animate-delay-100 card group p-7 flex flex-col items-stretch justify-between gap-7 border-white overflow-hidden relative
-                   rounded-none hover:bg-white hover:border-brand-active shadow-sm hover:shadow-xl hover:shadow-brand-shadow hover transition-all duration-300"
-                >
-                    <div class="text-lg text-black font-bold leading-none tracking-tight">Cash Reports<span class="transition-all duration-300 opacity-0 group-hover:opacity-100 text-brand-active">_</span></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 self-start group-hover:text-brand-active group-hover:scale-150 transition-all duration-300">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-                    </svg>
-                    <span class="bg-icon">
-                        <i class="ki-duotone ki-chart">
-                            <span class="path1"></span>
-                            <span class="path2"></span>
-                        </i>
-                    </span>
-                </a>
                 @php
+                    $cashReportsChildModules = $page->user->modules->where('parent_id', 1);
                     $modules = $page->user->modules->where('parent_id', 0)->sortBy('order');
                 @endphp
+                @if($cashReportsChildModules->isNotEmpty())
+                    <a href="{{ route('reports.' . $page->user->modules->sortBy('order')->first()->slug) }}"
+                       class="animate-in animate-delay-100 card group p-7 flex flex-col items-stretch justify-between gap-7 border-white overflow-hidden relative
+                                rounded-none hover:bg-white hover:border-brand-active shadow-sm hover:shadow-xl hover:shadow-brand-shadow hover transition-all duration-300"
+                    >
+                        <div class="text-lg text-black font-bold leading-none tracking-tight">Cash Reports<span class="transition-all duration-300 opacity-0 group-hover:opacity-100 text-brand-active">_</span></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 self-start group-hover:text-brand-active group-hover:scale-150 transition-all duration-300">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                        </svg>
+                        <span class="bg-icon">
+                            <i class="ki-duotone ki-chart">
+                                <span class="path1"></span>
+                                <span class="path2"></span>
+                            </i>
+                        </span>
+                    </a>
+                @endif
                 @foreach($modules as $module)
-                    <a href="{{ route('reports.' . $module->slug) }}" target="_blank"
+                    <a href="{{ route('reports.' . $module->slug) }}"
                        class="animate-in animate-delay-100 card group p-7 flex flex-col items-stretch justify-between gap-10 border-white overflow-hidden relative
                        rounded-none hover:bg-white hover:border-brand-active shadow-sm hover:shadow-xl hover:shadow-brand-shadow hover transition-all duration-300"
                     >
